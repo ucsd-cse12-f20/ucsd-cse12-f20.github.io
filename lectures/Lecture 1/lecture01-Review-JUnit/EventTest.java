a030207d8b7782960d061fc74da90e1349f1a648
Greg <59540827+ucsd-cse12-w20@users.noreply.github.com>
2020-01-07 10:53:28 -0800

    Create EventTest.java

---
+++ lectures/Lecture 1/lecture01-Review-JUnit/EventTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.time.LocalDateTime;
+
+public class EventTest {
+  
+
+	
+	
+
+  @Test
+  public void testConflict() {
+    
+	  
+
+	  
+	  
+  }
+}
+
